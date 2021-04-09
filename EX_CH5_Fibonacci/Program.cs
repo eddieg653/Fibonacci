09ec7b06d101fa9cab189b58ba057f76130822ad
Eddie Garcia <eddieg031@gmail.com>
2021-04-08 22:58:02 -0500

    Add project files.

---
+++ EX_CH5_Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EX_CH5_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentfib = 0;
+            int nexfib = 1;
+
+            Console.WriteLine(currentfib);
+            Console.WriteLine(nexfib);
+
+            for (int i = 0; i < 18; i++)
+            {
+                int NextNextfib = currentfib + nexfib;
+                Console.WriteLine(NextNextfib);
+                currentfib = nexfib;
+                nexfib = NextNextfib;
+            }
+        }
+    }
+}
